44eb3a4c05a05ff4b4157507ed0e49bc75c337f9
Matteo Kloiber <matteo.kloiber@me.com>
2014-07-08 21:45:26 +0200

    Init commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name = "java-select",
+    version = "1.0.0a",
+    description = "Command line script to easily switch a Java version.",
+    author = "Matteo Kloiber (Matt3o12)",
+    author_email = "info@matt3o12.de",
+    scripts = ["java-select.py"],
+)
