ea59eeea2e6a764feac30d09e349e46931ca43b4
Matteo Kloiber <matteo.kloiber@me.com>
2014-07-14 18:11:03 +0200

    Adding cmdline

---
+++ java_select/cmdline.py
@@ -0,0 +1,37 @@
+import sys
+import optparse
+import pdb
+
+class JavaSelectOptionParser(optparse.OptionParser, object):
+    def __init__(self):
+        super(JavaSelectOptionParser, self).__init__("%prog command [options]")
+        self.add_option("-v", "--version", action="store_true")
+
+class JavaSelectScript(object):
+    def __init__(self, args):
+        self.args = args
+        self.optParser = JavaSelectOptionParser()
+
+    def run(self):
+        options, args = self.optParser.parse_args(self.args)
+
+        if options.version:
+            self.showVersion()
+        elif len(args) != 1:
+            self.optParser.error("Changes the Java Home. Use 'java-select help' for help")
+        else:
+            raise optparse.OptionValueError("Invalid argument: {0}".format(args[0]))
+
+    def showVersion(self):
+        # TODO: Display correct version
+        print "Version: 0.0 (dev)"
+
+
+def main(args = None):
+    if args is None:
+        args = sys.argv[1:]
+
+    JavaSelectScript(args).run()
+
+if __name__ == "__main__":
+    main()
---
+++ tests/testcmdline.py
@@ -0,0 +1,61 @@
+import unittest
+from java_select.cmdline import *
+from nose_parameterized import parameterized
+
+from mock import *
+from StringIO import StringIO
+
+class TestJavaSelectOptionParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = JavaSelectOptionParser()
+
+    def testInit(self):
+        self.assertEquals("%prog command [options]", self.parser.usage)
+
+class TestJavaSelectScript(unittest.TestCase):
+    def setUp(self):
+        self.script = JavaSelectScript([])
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def testShowVersion(self, stdout):
+        # TODO: make this more dynamic
+        self.script.showVersion()
+        self.assertEquals("Version: 0.0 (dev)\n", stdout.getvalue())
+
+    def testRun_version(self):
+        self.script.showVersion = MagicMock(name="showVersion")
+        self.script.args = ['-v']
+        self.script.run()
+        self.script.showVersion.assert_called_once_with
+
+    @parameterized.expand([
+        [],
+        ["arg1", "arg2"],
+        ["arg1", "arg2", "arg3"]
+
+    ])
+    def testRun_help(self, *args):
+        error = MagicMock("error")
+        self.script.optParser.error = error
+        self.script.args = list(args)
+        self.script.run()
+        self.assertEquals(1, error.call_count)
+        self.assertEquals(1, len(error.call_args[0]))
+        self.assertIsInstance(error.call_args[0][0], basestring)
+
+    def testRun_invalidArgument(self):
+        self.script.args = ["Invalid_arg"]
+        self.assertRaises(optparse.OptionValueError, self.script.run)
+
+class TestLocalFunction(unittest.TestCase):
+    @patch("java_select.cmdline.JavaSelectScript")
+    @patch("sys.argv", ["script", "test", "args"])
+    def testMain_noArgs(self, JavaSelectScriptMock):
+        main()
+        JavaSelectScriptMock.assert_called_once_with(["test", "args"])
+
+    @patch("java_select.cmdline.JavaSelectScript")
+    def testMain(self, JavaSelectScriptMock):
+        args = ["hello", "world"]
+        main(args)
+        JavaSelectScriptMock.assert_called_once_with(["hello", "world"])
